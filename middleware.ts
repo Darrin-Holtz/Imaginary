f466d236bcfb2b47db0bf278340506a08f223660
Darrin-Holtz <darrinholtz@gmail.com>
2024-02-23 16:36:10 -0500

    error fix

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'],
+  ignoredRoutes: ['/no-auth-in-this-route'],
 });
  
 export const config = {
