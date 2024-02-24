915832e9294ac27ae15482aaf0587bcaa360a08e
Darrin-Holtz <darrinholtz@gmail.com>
2024-02-24 08:37:52 -0500

    switched functions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,19 +6,6 @@ import User from "../database/models/user.model";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
 
-// CREATE
-export async function createUser(user: CreateUserParams) {
-  try {
-    await connectToDatabase();
-
-    const newUser = await User.create(user);
-
-    return JSON.parse(JSON.stringify(newUser));
-  } catch (error) {
-    handleError(error);
-  }
-}
-
 // READ
 export async function getUserById(userId: string) {
   try {
@@ -29,6 +16,19 @@ export async function getUserById(userId: string) {
     if (!user) throw new Error("User not found");
 
     return JSON.parse(JSON.stringify(user));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// CREATE
+export async function createUser(user: CreateUserParams) {
+  try {
+    await connectToDatabase();
+
+    const newUser = await User.create(user);
+
+    return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
     handleError(error);
   }
