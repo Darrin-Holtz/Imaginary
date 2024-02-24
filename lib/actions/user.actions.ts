0164921a964a446b25ce98988e3f85a47d6d8089
Darrin-Holtz <darrinholtz@gmail.com>
2024-02-24 10:43:33 -0500

    rearranged code

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,6 +6,19 @@ import User from "../database/models/user.model";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
 
+// CREATE
+export async function createUser(user: CreateUserParams) {
+  try {
+    await connectToDatabase();
+
+    const newUser = await User.create(user);
+
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
 // READ
 export async function getUserById(userId: string) {
   try {
@@ -16,19 +29,6 @@ export async function getUserById(userId: string) {
     if (!user) throw new Error("User not found");
 
     return JSON.parse(JSON.stringify(user));
-  } catch (error) {
-    handleError(error);
-  }
-}
-
-// CREATE
-export async function createUser(user: CreateUserParams) {
-  try {
-    await connectToDatabase();
-
-    const newUser = await User.create(user);
-
-    return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
     handleError(error);
   }
