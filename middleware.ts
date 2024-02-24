d927786e8b75e5fc8d3e2cc216da0d48c8f140da
Darrin-Holtz <darrinholtz@gmail.com>
2024-02-24 13:32:02 -0500

    adjusting routes

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ["/", "/app/api/webhooks/clerk", "/transformations/add/recolor", "/transformations/add/remove"],
-  ignoredRoutes: ['/no-auth-in-this-route'],
+  publicRoutes: ["/", "/transformations/add/recolor", "/transformations/add/remove"],
+  ignoredRoutes: ["/app/api/webhooks/clerk"],
   debug: true
 });
  
