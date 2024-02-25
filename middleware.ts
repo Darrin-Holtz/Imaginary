2f4c5302ed24ffcb23d0af9d3432d968bcffc814
Darrin-Holtz <darrinholtz@gmail.com>
2024-02-25 17:04:44 -0500

    adjusting regex

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,9 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/stripe"],
   ignoredRoutes: [""],
+  // debug: true
 });
  
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.*\\..*|_next).*)', '/'],
 };
